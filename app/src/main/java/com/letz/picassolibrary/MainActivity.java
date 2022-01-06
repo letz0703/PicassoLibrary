93ce463a76764bf1e5f1802d67f33ddd35c85b8c
letz0703 <letz0703@gmail.com>
2022-01-06 14:23:02 +0900

    Picasso Setup

--- app/src/main/java/com/letz/picassolibrary/MainActivity.java
+++ app/src/main/java/com/letz/picassolibrary/MainActivity.java
@@ -3,13 +3,29 @@ package com.letz.picassolibrary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity
 {
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        image= findViewById(R.id.iv_Main);
+
+        String imageUrl="http://i.imgur.com/DvpvklR.png";
+        Picasso.get().load(imageUrl).into(image);
+
+        Picasso.get()
+                .load(imageUrl)
+                .resize(50, 50)
+                .centerCrop()
+                .into(image);
     }
+
 }
